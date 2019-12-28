7af2abbba34e19457fd8729d61f4b61d853d6455
PRASHANT PRABHU KARIYANNAVAR <54343627+prashk7@users.noreply.github.com>
2019-12-27 19:12:06 -0500

    Update thinback.groovy

--- thinbackup-plugin/thinback.groovy
+++ thinbackup-plugin/thinback.groovy
@@ -1,1 +1,16 @@
+import org.jvnet.hudson.plugins.thinbackup.ThinBackupMgmtLink;
 
+import org.jvnet.hudson.plugins.thinbackup.ThinBackupPluginImpl;
+
+String backupPath = "var/lib/username";
+String fullBackupSchedule = "0 13 * * 1-5";
+String diffBackupSchedule = "1 14 * * 1-5";
+boolean backupBuildResults= true
+
+
+
+ThinBackupPluginImpl thinBack = new ThinBackupPluginImpl();
+thinBack.setBackupPath(backupPath) 
+thinBack.setFullBackupSchedule(fullBackupSchedule)
+thinBack.setDiffBackupSchedule(diffBackupSchedule)
+thinBack.setBackupBuildResults(backupBuildResults)
